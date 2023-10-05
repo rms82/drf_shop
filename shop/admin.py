2762722554bf13a5729c351206dd735beee35c32
Reza <rdam9161@gmail.com>
2023-10-05 19:48:12 +0330

    ADD MODEL PRODUCT CATEGORY

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Categoty, Product
+
+
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'slug', 'inventory']
+    
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(blank=True)
+    image = models.ImageField(upload_to="product/img/", null=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.PositiveSmallIntegerField(default=0)
+    category = models.ManyToManyField(Category, related_name="products", blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(obj, *args, **kwargs):
+        obj.slug = slugify(obj.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.slug} - {self.inventory}"
